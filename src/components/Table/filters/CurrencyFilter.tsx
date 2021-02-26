849d4bf93bacd359f5e028919f85dc0bd5f8863d
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2021-02-26 12:44:47 +0100

    fix: Fix component definition to match our code style

--- src/components/Table/filters/CurrencyFilter.tsx
+++ src/components/Table/filters/CurrencyFilter.tsx
@@ -2,18 +2,14 @@ import React from "react";
 import Currency, { Props as CurrencyProps } from "@arteneo/forge/components/Form/fields/Currency";
 import FilterType from "@arteneo/forge/components/Table/definitions/FilterType";
 
-interface Props extends CurrencyProps {
+interface CurrencyFilterProps extends CurrencyProps {
     filterBy?: string;
     filterType?: FilterType;
 }
 
-// eslint-disable-next-line
-const CurrencyFilter: React.FC<Props> = ({ filterBy, filterType, ...props }: Props) => {
+const CurrencyFilter = ({ filterBy, filterType = "equal", ...props }: CurrencyFilterProps) => {
     return <Currency {...props} />;
 };
 
-CurrencyFilter.defaultProps = {
-    filterType: "equal",
-};
-
 export default CurrencyFilter;
+export { CurrencyFilterProps };
