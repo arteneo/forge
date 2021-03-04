2358a2137d93aeab3b55280e8890ce571c47d159
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2021-03-04 17:21:25 +0100

    feat(i-43): Add disableOnSuccessReload to ResultButtonEndpointConfirmation

--- src/components/Table/actions/result/ResultButtonEndpointConfirmation.tsx
+++ src/components/Table/actions/result/ResultButtonEndpointConfirmation.tsx
@@ -9,9 +9,11 @@ import { resolveAnyOrFunction } from "@arteneo/forge/utils/resolve";
 import { getIn } from "formik";
 import ResultInterface from "@arteneo/forge/components/Table/definitions/ResultInterface";
 import WrapperInterface from "@arteneo/forge/definitions/WrapperInterface";
+import { useTable } from "@arteneo/forge/components/Table/contexts/Table";
 
 interface EndpointConfirmationProps {
     requestConfig: TableResultActionResolveType<AxiosRequestConfig>;
+    disableOnSuccessReload?: boolean;
     onSuccess?: (
         defaultOnSuccess: () => void,
         response: AxiosResponse,
@@ -30,6 +32,7 @@ type ResultButtonEndpointConfirmationProps = Omit<ButtonEndpointConfirmationProp
 
 const ResultButtonEndpointConfirmation = ({
     requestConfig,
+    disableOnSuccessReload,
     onSuccess,
     result,
     field,
@@ -47,14 +50,25 @@ const ResultButtonEndpointConfirmation = ({
         throw new Error("ResultButtonEndpointConfirmation component: Missing required result prop");
     }
 
+    const { reload } = useTable();
     const value = path ? getIn(result, path) : result;
     const resolvedRequestConfig: AxiosRequestConfig = resolveAnyOrFunction(requestConfig, value, result, field);
 
+    const internalOnSuccess = (defaultOnSuccess: () => void) => {
+        defaultOnSuccess();
+
+        if (!disableOnSuccessReload) {
+            reload();
+        }
+    };
+
     // Lines below are written like that due to formatting issue
-    let resolvedOnSuccess = undefined;
+    let resolvedOnSuccess;
     if (onSuccess) {
         resolvedOnSuccess = (defaultOnSuccess: () => void, response: AxiosResponse) =>
-            onSuccess(defaultOnSuccess, response, value, result, field);
+            onSuccess(() => internalOnSuccess(defaultOnSuccess), response, value, result, field);
+    } else {
+        resolvedOnSuccess = internalOnSuccess;
     }
 
     return (
