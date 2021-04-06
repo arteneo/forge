e6933b9efac2f9598ce11499012e65ac24bb0d40
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2021-04-06 09:03:47 +0200

    feat(i-81): Make props in fieldProps for DateElement, DateTimeElement and TimeElement components optional

--- src/components/Form/elements/DateElement.tsx
+++ src/components/Form/elements/DateElement.tsx
@@ -16,7 +16,7 @@ interface DateElementSpecificProps {
         name: string,
         value?: string | null
     ) => void;
-    fieldProps?: KeyboardDatePickerProps;
+    fieldProps?: Partial<KeyboardDatePickerProps>;
 }
 
 type DateElementProps = DateElementSpecificProps & FieldElementPlaceholderInterface;
