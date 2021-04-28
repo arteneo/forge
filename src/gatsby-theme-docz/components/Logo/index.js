ad11a556bd789efb9235194c9dd6d930b41ef2ec
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2021-04-28 11:31:47 +0200

    feat(i-76): Move public assets to a folder. Fix .gitignore file

--- src/gatsby-theme-docz/components/Logo/index.js
+++ src/gatsby-theme-docz/components/Logo/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Logo = () => (
     <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
-        <img src="/logo.png" alt="Forge" style={{ height: "75%" }} />
+        <img src="/assets/images/logo.png" alt="Forge" style={{ height: "75%" }} />
         <h3 style={{ paddingLeft: "15px", margin: 0 }}>Forge</h3>
     </div>
 );
