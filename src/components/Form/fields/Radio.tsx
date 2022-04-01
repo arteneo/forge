5a43d6f0c78dd6ca0b43decbfc0184312bbeb649
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2022-04-01 16:19:42 +0200

    feat: Radio component

--- src/components/Form/fields/Radio.tsx
+++ src/components/Form/fields/Radio.tsx
@@ -1,83 +1,178 @@
 import React from "react";
-import * as Yup from "yup";
+import {
+    Radio as MuiRadio,
+    RadioGroup,
+    RadioGroupProps,
+    FormControl,
+    FormControlProps,
+    FormHelperText,
+    FormLabel,
+    FormLabelProps,
+    FormControlLabel,
+    FormControlLabelProps,
+    IconButton,
+    Tooltip,
+} from "@mui/material";
+import { Close } from "@mui/icons-material";
+import { FormikValues, FormikProps, useFormikContext, getIn } from "formik";
+import { useTranslation } from "react-i18next";
 import { useForm } from "../../../components/Form/contexts/Form";
-import { resolveBooleanOrFunction } from "../../../utils/resolve";
-import { FormikValues, FormikProps, useFormikContext } from "formik";
-import RadioElement, { RadioElementSpecificProps } from "../../../components/Form/elements/RadioElement";
-import TextFieldInterface from "../../../components/Form/definitions/TextFieldInterface";
+import OptionsType from "../../../components/Form/definitions/OptionsType";
+import FieldInterface from "../../../components/Form/definitions/FieldInterface";
 
-type RadioProps = RadioElementSpecificProps & TextFieldInterface;
+interface RadioSpecificProps {
+    options: OptionsType;
+    disableTranslateOption?: boolean;
+    enableClear?: boolean;
+    clearLabel?: string;
+    onChange?: (
+        path: string,
+        // eslint-disable-next-line
+        setFieldValue: (field: string, value: any, shouldValidate?: boolean) => void,
+        event: React.ChangeEvent<HTMLInputElement>,
+        value: string,
+        onChange: () => void,
+        values: FormikValues,
+        name: string
+    ) => void;
+    formLabelProps?: FormLabelProps;
+    radioGroupProps?: RadioGroupProps;
+    formControlLabelProps?: FormControlLabelProps;
+    formControlProps?: FormControlProps;
+}
+
+type RadioProps = RadioSpecificProps & FieldInterface;
 
 const Radio = ({
-    name,
-    path,
-    label,
-    disableAutoLabel = false,
-    disableTranslateLabel = false,
-    help,
-    disableTranslateHelp = false,
-    required = false,
-    hidden = false,
-    disabled = false,
-    validationSchema,
-    ...elementSpecificProps
+    options,
+    disableTranslateOption,
+    enableClear = false,
+    clearLabel = "label.radioClear",
+    onChange,
+    formLabelProps,
+    radioGroupProps,
+    formControlLabelProps,
+    formControlProps,
+    ...field
 }: RadioProps) => {
-    if (typeof name === "undefined") {
-        throw new Error("Radio component: name is required prop. By default it is injected by FormContent.");
-    }
+    const { t } = useTranslation();
+    const {
+        values,
+        touched,
+        errors,
+        submitCount,
+        setFieldValue,
+        registerField,
+        unregisterField,
+    }: FormikProps<FormikValues> = useFormikContext();
+    const { resolveField } = useForm();
+    const { name, path, label, error, hasError, help, required, disabled, hidden, validate } = resolveField({
+        values,
+        touched,
+        errors,
+        submitCount,
+        ...field,
+    });
 
-    const { getError, getLabel, getHelp } = useForm();
-    const { values, touched, errors, submitCount }: FormikProps<FormikValues> = useFormikContext();
-
-    const resolvedRequired = resolveBooleanOrFunction(required, values, touched, errors, name);
-    const resolvedHidden = resolveBooleanOrFunction(hidden, values, touched, errors, name);
-    const resolvedPath = path ? path : name;
-
-    React.useEffect(() => updateValidationSchema(), [resolvedRequired, resolvedHidden]);
-
-    const updateValidationSchema = () => {
-        let defaultValidationSchema = Yup.string();
-
-        if (resolvedRequired) {
-            defaultValidationSchema = defaultValidationSchema.required("validation.required");
+    React.useEffect(() => {
+        if (hidden || typeof validate === "undefined") {
+            return;
         }
 
-        // TODO
-        // resolveValidationSchema(
-        //     resolvedPath,
-        //     validationSchema,
-        //     defaultValidationSchema,
-        //     resolvedHidden,
-        //     resolvedRequired,
-        //     values,
-        //     touched,
-        //     errors,
-        //     name
-        // );
-    };
+        registerField(name, {
+            validate: () => validate,
+        });
 
-    if (resolvedHidden) {
+        return () => {
+            unregisterField(name);
+        };
+    }, [hidden, registerField, unregisterField, name, validate]);
+
+    if (hidden) {
         return null;
     }
 
-    const resolvedHelp = getHelp(values, touched, errors, name, help, disableTranslateHelp);
-    const resolvedError = getError(resolvedPath, touched, errors, submitCount);
-    const resolvedDisabled = resolveBooleanOrFunction(disabled, values, touched, errors, name);
-    const resolvedLabel = getLabel(label, values, touched, errors, name, disableAutoLabel, disableTranslateLabel);
+    const defaultOnChange = (event: React.ChangeEvent<HTMLInputElement>, value: string) => {
+        setFieldValue(path, value);
+    };
+
+    const callableOnChange = (event: React.ChangeEvent<HTMLInputElement>, value: string) => {
+        if (onChange) {
+            onChange(path, setFieldValue, event, value, () => defaultOnChange(event, value), values, name);
+            return;
+        }
+
+        defaultOnChange(event, value);
+    };
+
+    const internalFormControlProps: FormControlProps = {
+        error: hasError,
+    };
+    const mergedFormControlProps = Object.assign(internalFormControlProps, formControlProps);
+
+    const internalRadioGroupProps: RadioGroupProps = {
+        value: getIn(values, path, ""),
+        row: true,
+        onChange: callableOnChange,
+    };
+    const mergedRadioGroupProps = Object.assign(internalRadioGroupProps, radioGroupProps);
+
+    const internalFormLabelProps: FormLabelProps = {
+        required,
+    };
+    const mergedFormLabelProps = Object.assign(internalFormLabelProps, formLabelProps);
+
+    let helperText: undefined | React.ReactNode = undefined;
+
+    if (hasError || help) {
+        helperText = (
+            <>
+                {error}
+                {hasError && <br />}
+                {help}
+            </>
+        );
+    }
+
+    let clearComponent: null | React.ReactElement = null;
+    if (enableClear) {
+        const clearValue = () => {
+            setFieldValue(path, undefined);
+        };
+
+        clearComponent = (
+            <IconButton onClick={() => clearValue()}>
+                <Close />
+            </IconButton>
+        );
+
+        if (typeof clearLabel !== "undefined") {
+            // t(clearLabel) "?? clearLabel" is just for TypeScript
+            clearComponent = <Tooltip title={t(clearLabel) ?? clearLabel}>{clearComponent}</Tooltip>;
+        }
+    }
 
     return (
-        <RadioElement
-            {...{
-                name,
-                path: resolvedPath,
-                label: resolvedLabel,
-                error: resolvedError,
-                help: resolvedHelp,
-                required: resolvedRequired,
-                disabled: resolvedDisabled,
-                ...elementSpecificProps,
-            }}
-        />
+        <FormControl {...mergedFormControlProps}>
+            {label && <FormLabel {...mergedFormLabelProps}>{label}</FormLabel>}
+            <RadioGroup {...mergedRadioGroupProps}>
+                {options.map((option, key) => (
+                    <FormControlLabel
+                        key={key}
+                        {...{
+                            value: String(option.id),
+                            control: <MuiRadio required={required} disabled={disabled} />,
+                            label: disableTranslateOption
+                                ? option.representation // as string is just for TypeScript
+                                : (t(option.representation) as string),
+                            ...formControlLabelProps,
+                        }}
+                    />
+                ))}
+                {clearComponent}
+            </RadioGroup>
+            {helperText && <FormHelperText>{helperText}</FormHelperText>}
+        </FormControl>
     );
 };
 
