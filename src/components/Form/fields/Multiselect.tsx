59b8e107b8f05eeac8cadfe8ee044158f089f4d0
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2022-04-01 15:33:42 +0200

    feat: Multiselect component

--- src/components/Form/fields/Multiselect.tsx
+++ src/components/Form/fields/Multiselect.tsx
@@ -1,100 +1,237 @@
 import React from "react";
-import * as Yup from "yup";
+import {
+    Checkbox as MuiCheckbox,
+    TextField as MuiTextField,
+    FormControl,
+    FormControlProps,
+    FormHelperText,
+    Autocomplete,
+    AutocompleteChangeReason,
+    AutocompleteChangeDetails,
+    AutocompleteProps,
+    AutocompleteRenderInputParams,
+} from "@mui/material";
+import { CheckBox, CheckBoxOutlineBlank } from "@mui/icons-material";
+import { useTranslation } from "react-i18next";
 import { useForm } from "../../../components/Form/contexts/Form";
-import { resolveBooleanOrFunction } from "../../../utils/resolve";
-import { FormikValues, FormikProps, useFormikContext } from "formik";
-import MultiselectElement, {
-    MultiselectElementSpecificProps,
-} from "../../../components/Form/elements/MultiselectElement";
-import TextFieldPlaceholderInterface from "../../../components/Form/definitions/TextFieldPlaceholderInterface";
+import { FormikValues, FormikProps, useFormikContext, getIn } from "formik";
+import OptionsType from "../../../components/Form/definitions/OptionsType";
+import OptionInterface from "../../../components/Form/definitions/OptionInterface";
+import FieldPlaceholderInterface from "../../../components/Form/definitions/FieldPlaceholderInterface";
+import {
+    SelectValueType,
+    Multiple,
+    DisableClearable,
+    FreeSolo,
+} from "../../../components/Form/definitions/AutocompleteTypes";
 
-type MultiselectProps = MultiselectElementSpecificProps & TextFieldPlaceholderInterface;
+type MultiselectAutocompleteProps = AutocompleteProps<OptionInterface, Multiple, DisableClearable, FreeSolo>;
+type MultiselectAutocompletePartialProps<T> = {
+    [P in keyof T]?: T[P];
+};
+// We need to allow passing autocomplete props without required ones. They are filled by component.
+type MultiselectAutocompleteOptionalProps = MultiselectAutocompletePartialProps<MultiselectAutocompleteProps>;
+
+interface MultiselectSpecificProps {
+    options: OptionsType;
+    disableTranslateOption?: boolean;
+    onChange?: (
+        path: string,
+        // eslint-disable-next-line
+        setFieldValue: (field: string, value: any, shouldValidate?: boolean) => void,
+        value: SelectValueType,
+        onChange: () => void,
+        values: FormikValues,
+        event: React.SyntheticEvent,
+        reason: AutocompleteChangeReason,
+        name: string,
+        details?: AutocompleteChangeDetails<OptionInterface>
+    ) => void;
+    groupBy?: (option: OptionInterface) => string;
+    disableTranslateGroupBy?: boolean;
+    autocompleteProps?: MultiselectAutocompleteOptionalProps;
+    formControlProps?: FormControlProps;
+}
+
+type MultiselectProps = MultiselectSpecificProps & FieldPlaceholderInterface;
+
+const MultiselectRenderInput = (params: AutocompleteRenderInputParams) => <MuiTextField {...params} />;
 
 const Multiselect = ({
-    name,
-    path,
-    label,
-    placeholder,
-    disableAutoLabel = false,
-    disableTranslateLabel = false,
-    enableAutoPlaceholder = false,
-    disableTranslatePlaceholder = false,
-    help,
-    disableTranslateHelp = false,
-    required = false,
-    hidden = false,
-    disabled = false,
-    validationSchema,
-    ...elementSpecificProps
+    options,
+    disableTranslateOption,
+    onChange,
+    groupBy,
+    disableTranslateGroupBy,
+    autocompleteProps,
+    formControlProps,
+    ...field
 }: MultiselectProps) => {
-    if (typeof name === "undefined") {
-        throw new Error("Multiselect component: name is required prop. By default it is injected by FormContent.");
-    }
-
-    const { getError, getLabel, getPlaceholder, getHelp } = useForm();
-    const { values, touched, errors, submitCount }: FormikProps<FormikValues> = useFormikContext();
-
-    const resolvedRequired = resolveBooleanOrFunction(required, values, touched, errors, name);
-    const resolvedHidden = resolveBooleanOrFunction(hidden, values, touched, errors, name);
-    const resolvedPath = path ? path : name;
-
-    React.useEffect(() => updateValidationSchema(), [resolvedRequired, resolvedHidden]);
-
-    const updateValidationSchema = () => {
-        let defaultValidationSchema = Yup.array();
-
-        if (resolvedRequired) {
-            defaultValidationSchema = defaultValidationSchema
-                .min(1, "validation.required")
-                .required("validation.required");
-        }
-
-        // TODO
-        // resolveValidationSchema(
-        //     resolvedPath,
-        //     validationSchema,
-        //     defaultValidationSchema,
-        //     resolvedHidden,
-        //     resolvedRequired,
-        //     values,
-        //     touched,
-        //     errors,
-        //     name
-        // );
-    };
-
-    if (resolvedHidden) {
-        return null;
-    }
-
-    const resolvedHelp = getHelp(values, touched, errors, name, help, disableTranslateHelp);
-    const resolvedError = getError(resolvedPath, touched, errors, submitCount);
-    const resolvedDisabled = resolveBooleanOrFunction(disabled, values, touched, errors, name);
-    const resolvedLabel = getLabel(label, values, touched, errors, name, disableAutoLabel, disableTranslateLabel);
-    const resolvedPlaceholder = getPlaceholder(
-        placeholder,
+    const { t } = useTranslation();
+    const {
         values,
         touched,
         errors,
-        name,
-        enableAutoPlaceholder,
-        disableTranslatePlaceholder
-    );
+        submitCount,
+        setFieldValue,
+        registerField,
+        unregisterField,
+    }: FormikProps<FormikValues> = useFormikContext();
+    const { resolvePlaceholderField } = useForm();
+    const { name, path, label, error, hasError, help, required, disabled, hidden, validate, placeholder } =
+        resolvePlaceholderField({
+            values,
+            touched,
+            errors,
+            submitCount,
+            ...field,
+        });
 
-    return (
-        <MultiselectElement
-            {...{
+    React.useEffect(() => {
+        if (hidden || typeof validate === "undefined") {
+            return;
+        }
+
+        registerField(name, {
+            validate: () => validate,
+        });
+
+        return () => {
+            unregisterField(name);
+        };
+    }, [hidden, registerField, unregisterField, name, validate]);
+
+    if (hidden) {
+        return null;
+    }
+
+    const defaultOnChange = (event: React.SyntheticEvent, value: SelectValueType, reason: AutocompleteChangeReason) => {
+        if (reason === "clear") {
+            setFieldValue(path, "");
+            return;
+        }
+
+        if (!Array.isArray(value)) {
+            return;
+        }
+
+        const newValues = value.map((selectedOption) => {
+            if (typeof selectedOption === "string") {
+                return;
+            }
+
+            return selectedOption.id;
+        });
+        setFieldValue(path, newValues);
+    };
+
+    const callableOnChange = (
+        event: React.SyntheticEvent,
+        value: SelectValueType,
+        reason: AutocompleteChangeReason,
+        details?: AutocompleteChangeDetails<OptionInterface>
+    ) => {
+        if (onChange) {
+            // Parameters are swapped for convenience
+            onChange(
+                path,
+                setFieldValue,
+                value,
+                () => defaultOnChange(event, value, reason),
+                values,
+                event,
+                reason,
                 name,
-                path: resolvedPath,
-                label: resolvedLabel,
-                placeholder: resolvedPlaceholder,
-                error: resolvedError,
-                help: resolvedHelp,
-                required: resolvedRequired,
-                disabled: resolvedDisabled,
-                ...elementSpecificProps,
+                details
+            );
+            return;
+        }
+
+        defaultOnChange(event, value, reason);
+    };
+
+    const value = getIn(values, path, []);
+    const internalFormControlProps: FormControlProps = {
+        error: hasError,
+    };
+    const mergedFormControlProps = Object.assign(internalFormControlProps, formControlProps);
+
+    const renderInput = (params: AutocompleteRenderInputParams) => (
+        // inputProps are additionally processed and set required prop based on currently selected values
+        // This allows to allows proper submitting when required = true
+        <MultiselectRenderInput
+            {...{
+                label,
+                required,
+                placeholder,
+                error: hasError,
+                ...params,
+                inputProps: {
+                    ...params.inputProps,
+                    required: required ? value.length === 0 : false,
+                },
             }}
         />
+    );
+
+    const internalAutocompleteProps: MultiselectAutocompleteProps = {
+        renderInput,
+        options,
+        value: [],
+        multiple: true,
+        loadingText: t("placeholder.loading"),
+        noOptionsText: t("placeholder.selectMultipleEmpty"),
+        disableCloseOnSelect: true,
+        getOptionLabel: (option: OptionInterface) =>
+            disableTranslateOption ? option.representation : t(option.representation),
+        disabled,
+        onChange: callableOnChange,
+        renderOption: (props, option, { selected }) => (
+            <li {...props}>
+                <MuiCheckbox
+                    {...{
+                        icon: <CheckBoxOutlineBlank {...{ fontSize: "small" }} />,
+                        checkedIcon: <CheckBox {...{ fontSize: "small" }} />,
+                        style: { marginRight: 8 },
+                        checked: selected,
+                    }}
+                />
+                {disableTranslateOption ? option.representation : t(option.representation)}
+            </li>
+        ),
+    };
+
+    if (typeof groupBy !== "undefined") {
+        internalAutocompleteProps.groupBy = (option: OptionInterface) =>
+            disableTranslateGroupBy ? groupBy(option) : t(groupBy(option));
+    }
+
+    if (typeof value !== "undefined") {
+        const optionSelected = options.filter((option) => {
+            return value.includes(option.id);
+        });
+        internalAutocompleteProps.value = optionSelected ?? [];
+    }
+
+    const mergedAutocompleteProps = Object.assign(internalAutocompleteProps, autocompleteProps);
+
+    let helperText: undefined | React.ReactNode = undefined;
+
+    if (hasError || help) {
+        helperText = (
+            <>
+                {error}
+                {hasError && <br />}
+                {help}
+            </>
+        );
+    }
+
+    return (
+        <FormControl {...mergedFormControlProps}>
+            <Autocomplete {...mergedAutocompleteProps} />
+            {helperText && <FormHelperText>{helperText}</FormHelperText>}
+        </FormControl>
     );
 };
 
