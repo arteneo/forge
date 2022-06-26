b6b7acba0ffb09c6662d0ff221b02edad8d93447
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2022-06-26 11:46:28 +0200

    feat(sma-617): Update Create to adjust to react router v6

--- src/components/Table/toolbar/Create.tsx
+++ src/components/Table/toolbar/Create.tsx
@@ -1,24 +1,15 @@
 import React from "react";
-import { useTable } from "../../../components/Table/contexts/Table";
 import ButtonLink, { ButtonLinkProps } from "../../../components/Common/ButtonLink";
 import Optional from "../../../definitions/Optional";
 
 type CreateProps = Optional<ButtonLinkProps, "to">;
 
-const Create = ({ to, ...props }: CreateProps) => {
-    const { custom } = useTable();
-
-    if (typeof to === "undefined" && typeof custom?.paths?.create === "undefined") {
-        throw new Error(
-            "Create component: Missing required to prop or paths.create definition in custom variable used by Table context"
-        );
-    }
-
+const Create = (props: CreateProps) => {
     return (
         <ButtonLink
             {...{
                 label: "action.create",
-                to: to ? to : custom.paths.create,
+                to: "../create",
                 color: "primary",
                 variant: "contained",
                 ...props,
